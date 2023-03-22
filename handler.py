add5354a162a7a5ce185a5c909e28ddd8a307917
PiotrKuligowski <piotr.kuligowski98@gmail.com>
2023-03-22 18:06:01 +0100

    Disabled flushing tables

--- handler.py
+++ handler.py
@@ -6,6 +6,7 @@ import time
 asg_client = boto3.client('autoscaling', region_name=os.getenv('REGION'))
 ec2_client = boto3.client('ec2', region_name=os.getenv('REGION'))
 
+
 def get_volume_status():
     return ec2_client.describe_volumes(
         VolumeIds=[os.getenv('VOLUME_ID')]
@@ -25,26 +26,26 @@ def lambda_handler(event, context):
             'body': None
         }
 
-    asg_instance_ip = ec2_client.describe_instances(
-        InstanceIds=[asg_instance_id]
-    )['Reservations'][0]['Instances'][0]['PublicIpAddress']
-
-    try:
-        conn = pymysql.connect(
-            host=asg_instance_ip,
-            user=os.getenv("MYSQL_USER"),
-            passwd=os.getenv("MYSQL_PASSWORD"),
-            db=os.getenv("MYSQL_DATABASE"),
-            connect_timeout=5
-        )
-
-        with conn.cursor() as cur:
-            cur.execute("FLUSH TABLES WITH READ LOCK")
-            cur.execute("SHUTDOWN")
-        conn.commit()
-
-    except pymysql.MySQLError as e:
-        print(f"ERROR: Unexpected error: Could not connect to MySQL instance. Error: {e}")
+    # asg_instance_ip = ec2_client.describe_instances(
+    #     InstanceIds=[asg_instance_id]
+    # )['Reservations'][0]['Instances'][0]['PublicIpAddress']
+    #
+    # try:
+    #     conn = pymysql.connect(
+    #         host=asg_instance_ip,
+    #         user=os.getenv("MYSQL_USER"),
+    #         passwd=os.getenv("MYSQL_PASSWORD"),
+    #         db=os.getenv("MYSQL_DATABASE"),
+    #         connect_timeout=5
+    #     )
+    #
+    #     with conn.cursor() as cur:
+    #         cur.execute("FLUSH TABLES WITH READ LOCK")
+    #         cur.execute("SHUTDOWN")
+    #     conn.commit()
+    #
+    # except pymysql.MySQLError as e:
+    #     print(f"ERROR: Unexpected error: Could not connect to MySQL instance. Error: {e}")
 
     response = ec2_client.detach_volume(
         Device='/dev/sdh',
